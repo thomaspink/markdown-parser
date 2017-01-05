e2cae58202c5cf383c2942113f2e38577e77ce5d
Thomas Pink <thomas.pink@netural.com>
2017-01-05 19:19:23 +0100

    changed timeout in demo app, removed log

--- src/demo-app/index.ts
+++ src/demo-app/index.ts
@@ -2,14 +2,13 @@ const textEl = document.querySelector('textarea');
 const resultEl = document.querySelector('.result');
 
 function updateResult() {
-  console.log('update');
   resultEl.innerHTML = textEl.value;
 }
 
 let timer = 0;
 textEl.addEventListener('input', (event) => {
   clearTimeout(timer);
-  timer = setTimeout(updateResult, 150);
+  timer = setTimeout(updateResult, 300);
 });
 
 updateResult();
